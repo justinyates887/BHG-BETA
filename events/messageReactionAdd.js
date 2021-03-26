4fcfb202c46d5b60ad2ff8caa0e6169d0c497d30
justinyates887 <justinyates887@gmail.com>
2021-03-26 08:34:18 -0500

    Started reaction roles

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,3 +1,23 @@
+const { MessageReaction, User } = require('discord.js')
+const reactionSchema = require('../commands/setup/schemas/reaction-schema')
+/**
+ * 
+ * @param {MessageReaction} reaction 
+ * @param {User} user 
+ */
 
 module.exports = async (client, reaction, user) => {
+    reactionSchema.findOne({
+        _id: reaction.message.guild.id,
+        mID: reaction.message.id,
+        reaction: reaction.emoji.toString(),
+
+    }, async(err, data) => {
+        if(err) throw err;
+        if(data){
+            if(reaction.message.member.roles.cache.has(data.role)){};
+        } else {
+            reaction.message.member.roles.add(data.role)
+        }
+    })
 }
