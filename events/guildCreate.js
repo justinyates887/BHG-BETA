6e838d32977d6c7055432def9ab4ecee848fbdfa
Justin Yates <justinyates887@gmail.com>
2021-03-28 12:28:37 -0700

    sometimes i hate this bot

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -9,17 +9,15 @@ module.exports = async (client, guild) => {
           if (channel.type === "text") {
             if (channel.permissionsFor(client.user).has("VIEW_CHANNEL") === true) {
               if (channel.permissionsFor(client.user).has("SEND_MESSAGES") === true) {
-                if (config.embeds === true) { 
-                    let embed = new Discord.MessageEmbed()
-                        .setAuthor("Hello!")
-                        .setColor("#486dAA") 
-                        .setDescription("Thank you for inviting me!\n\n\
-                        To see a full list of commands use !help\n\n\
-                        To follow production visit us at https://github.com/justinyates887/blue-haired-girl-bot \n\n\
-                        To report a bug please message \
-                        @Protest#9576, @Mr.Floyd 🇨🇦#0420 or join the Official Discord Channel: https://discord.gg/tb4mZWtXC8")
-                         channel.send(embed);
-                }
+                  let embed = new Discord.MessageEmbed()
+                      .setAuthor("Hello!")
+                      .setColor("#486dAA") 
+                      .setDescription("Thank you for inviting me!\n\n\
+                      To see a full list of commands use b!help\n\n\
+                      To follow production visit us at https://github.com/justinyates887/BHG-BETA \n\n\
+                      To report a bug please message \
+                      @Protest#9576, @Mr.Floyd 🇨🇦#0420 or join the Official Discord Channel: https://discord.gg/tb4mZWtXC8")
+                        channel.send(embed);
                 return found = 1;
               }
             }
